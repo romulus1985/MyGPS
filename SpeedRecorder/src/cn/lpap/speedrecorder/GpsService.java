5464625a9b75bcd9a9156f5ef074c29ca226a9bd
romulus <287306752@qq.com>
2016-09-26 20:42:59 +0800

    Add boot completed service: GpsService

---
+++ SpeedRecorder/src/cn/lpap/speedrecorder/GpsService.java
@@ -0,0 +1,20 @@
+package cn.lpap.speedrecorder;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class GpsService extends Service {
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
