daa06148b794a59102f152a5897cf65401241508
root <root@romulus-pc.(none)>
2016-09-26 11:00:00 +0800

    Export database to /sdcard/

--- AntiOverSpeed/src/cn/lpap/antioverspeed/AppInfo.java
+++ AntiOverSpeed/src/cn/lpap/antioverspeed/AppInfo.java
@@ -1,15 +1,25 @@
 package cn.lpap.antioverspeed;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AppInfo extends Activity {
     private Button mQueryPositions = null;
     private Button mSelectTime = null;
+    private Button mExportDB = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +48,55 @@ public class AppInfo extends Activity {
 				startActivity(selectTime);
 			}
 		});
+		mExportDB = (Button)findViewById(R.id.export_db);
+		mExportDB.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				final String oldPath = "/data/data/" + getPackageName() + "/databases/" + GpsDatabaseHelper.DB_NAME;
+				final String newPath = Environment.getExternalStorageDirectory() + File.separator + GpsDatabaseHelper.DB_NAME;
+				copyFile(oldPath, newPath);
+			}
+		});
+	}
+	
+	private void copyFile(final String oldPath,
+			final String newPath) {
+		int byteSum = 0;
+		int byteRead = 0;
+		InputStream input = null;
+		OutputStream output = null;
+		try{
+			File oldFile = new File(oldPath);
+			File newFile = new File(newPath);
+			if(newFile.exists()) {
+				newFile.delete();
+			} 
+			newFile.createNewFile();
+			if(!oldFile.exists()) {
+				return;
+			}
+			input = new FileInputStream(oldFile);
+			output = new FileOutputStream(newFile);
+			byte[] buffer = new byte[1024];
+			while(-1 != (byteRead = input.read(buffer))) {
+				byteSum += byteRead;
+				output.write(buffer, 0, byteRead);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(null != input) {
+				input.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		Toast.makeText(this, R.string.export_db_ok, Toast.LENGTH_SHORT).show();
 	}
 }
--- AntiOverSpeed/src/cn/lpap/antioverspeed/GpsDatabaseHelper.java
+++ AntiOverSpeed/src/cn/lpap/antioverspeed/GpsDatabaseHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class GpsDatabaseHelper extends SQLiteOpenHelper {
-    private static final String name = "Gps"; //数据库名称  
+    public static final String DB_NAME = "Gps"; //数据库名称  
     
     private static final int version = 1; //数据库版本  
     private static final String TABLE_NAME = "position";
@@ -21,7 +21,7 @@ public class GpsDatabaseHelper extends SQLiteOpenHelper {
 		  
         //第三个参数CursorFactory指定在执行查询时获得一个游标实例的工厂类,设置为null,代表使用系统默认的工厂类  
 
-          super(context, name, null, version);  
+          super(context, DB_NAME, null, version);  
 
    }  
 
