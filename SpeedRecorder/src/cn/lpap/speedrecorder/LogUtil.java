65dd6971baf9e6af304b6ec02b6a07930cc5ae5f
root <root@romulus-pc.(none)>
2016-09-28 11:26:31 +0800

    Add buffer and timer for log.

--- SpeedRecorder/src/cn/lpap/speedrecorder/LogUtil.java
+++ SpeedRecorder/src/cn/lpap/speedrecorder/LogUtil.java
@@ -10,37 +10,50 @@ import java.util.Date;
 public class LogUtil {
 	private static final String LOG_PREFIX = "log";
 	private static final String LOG_EXT = ".txt";
+	private static StringBuffer sBuffer = new StringBuffer();
+	private static int BUFF_LEN = 1024;
+	
+	private static long sLogTime = System.currentTimeMillis();
+	private static long LOG_INTERVAL = 1000 * 10;
 	public static void log(final String msg) {
-		Date date = new Date();
-		String path = Utils.getExportFolder() 
-				+ File.separator + LOG_PREFIX + Utils.getUniqueHourName()
-				+ LOG_EXT;
-		File log = new File(path);
-		FileOutputStream output = null;
-		BufferedWriter buff = null;
-		try {
-			if(!log.exists()) {
-				log.createNewFile();
-			}
-			output = new FileOutputStream(log, true);
-			buff = new BufferedWriter(new OutputStreamWriter(output));
-			buff.write(msg + "\n");
-			buff.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
+		sBuffer.append(msg + "\n");
+		long cur = System.currentTimeMillis();
+		if(BUFF_LEN < sBuffer.length()
+				|| LOG_INTERVAL < (cur - sLogTime)) {
+			Date date = new Date();
+			String path = Utils.getExportFolder() 
+					+ File.separator + LOG_PREFIX + Utils.getUniqueHourName()
+					+ LOG_EXT;
+			File log = new File(path);
+			FileOutputStream output = null;
+			BufferedWriter buff = null;
 			try {
-				if(null != buff) {
-					buff.close();
+				if(!log.exists()) {
+					log.createNewFile();
 				}
-				if(null != output) {
-					output.close();
-				}
+				output = new FileOutputStream(log, true);
+				buff = new BufferedWriter(new OutputStreamWriter(output));
+				buff.write(sBuffer.toString());
+				buff.flush();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				try {
+					if(null != buff) {
+						buff.close();
+					}
+					if(null != output) {
+						output.close();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
+			sLogTime = System.currentTimeMillis();
+			// clear
+			sBuffer.delete(0, sBuffer.length() -1);
 		}
 	}
 }
